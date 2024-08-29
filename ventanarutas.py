bc5ed1890e7293406746733a3e61712cfaa03d5e
Enrique Trujillo <lt11003@ues.edu.sv>
2024-08-29 10:57:05 -0600

    agreglo
    agregue seleccion de rutas

--- ventanarutas.py
+++ ventanarutas.py
@@ -4,9 +4,6 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
-cantidaprint = int(print("Ingrese la cantidad de Carpetas a utilizar"))
-                  
-while conteo<=cantidaprint:
-    ruta = filedialog.askdirectory(title ="Selecciona carpteta")
-conteo= conteo+1
+ruta = filedialog.askdirectory(title ="Selecciona carpteta")
+
 print("Ruta seleccionada:", ruta)
