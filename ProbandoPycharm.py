522d124b028bc11e5b7b09382192c55d75977d28
Enrique Trujillo <lt11003@ues.edu.sv>
2024-08-29 10:53:52 -0600

    Agregado seleccion de rutas

--- ProbandoPycharm.py
+++ ProbandoPycharm.py
@@ -1,32 +1,31 @@
 import os
 import ventanarutas
 
-directorio = "C:/Users/enri_"
+directorio = ventanarutas.ruta
 archivos = os.listdir(directorio)
-
-def __archivosdir__():
+listax = []
+def archivosdir():
     count = 0
     nombrecarpetas = []
     listax = []
     for item in archivos:
-        if item.startswith(".") or (item.__len__()>=15):
+        if item.startswith(".") or (item.__len__()>=15) or item.startswith("$"):
             listax.append(item)
             count +=1
         else:
             #print(item + " \n")
             nombrecarpetas.append(item)
-    #print(listado)
-    #print(listax)
-    print("total de elementos sistema no incluidos",count)
+    #print(nombrecarpetas)
+    #print("total de elementos sistema no incluidos",count)
     return nombrecarpetas
 
 
 
-def __buscandoraiz__(lista):
+def buscandoraiz(lista):
     for item in lista:
         direcc = directorio+'/'+item
         #print(direcc)
 
-listado = __archivosdir__()
-__buscandoraiz__(listado)
+listado = archivosdir()
+buscandoraiz(listado)
 
